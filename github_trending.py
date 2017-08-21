11e442891fb52cbea96ff746160c83c87dc646b7
velios <prostozapomnit@gmail.com>
2017-08-21 21:08:27 +0300

    Fix inspector comments
    Remove global variables
    Remove commented code
    Simplified loop with list comprehension
    Remove magic numbers

--- github_trending.py
+++ github_trending.py
@@ -2,44 +2,41 @@ import requests
 from datetime import date
 from datetime import timedelta
 
-GITHUB_API_URI='https://api.github.com'
-
 
 def get_offset_date(modifed_date, offset_in_days):
     return date.isoformat(modifed_date + timedelta(days=int(offset_in_days)))
 
 
 def get_trending_repositories(start_search_date, number_of_results=20):
-    query_search_url = '{}/search/repositories'.format(GITHUB_API_URI)
-    query_parameters = {'q':'created:>{}'.format(start_search_date),
+    github_api_uri = 'https://api.github.com'
+    query_search_url = '{}/search/repositories'.format(github_api_uri)
+    query_parameters = {'q': 'created:>{}'.format(start_search_date),
                         'sort': 'stars',
                         'order': 'desc',
                         'per_page': number_of_results}
-    trending_repositories_json = requests.get(query_search_url,
-                                              query_parameters).json()['items']
-    short_list = []
-    for repository in trending_repositories_json:
-        repo_name = repository['name']
-        repo_owner = repository['owner']['login']
-        #number_of_open_issues = get_open_issues_amount(repo_owner, repo_name)
-        short_list.append({'repo_name': str(repo_name),
-                           'repo_owner': str(repo_owner),
-                           'stars': repository['stargazers_count'],
-                           'issues': repository['open_issues'],
-                           'url': repository['html_url']
-                           })
-    return short_list
+    trending_repositories_json_list = requests.get(query_search_url,
+                                                   query_parameters).json()['items']
+    result_trending_list = []
+    for repository in trending_repositories_json_list:
+        repository_name = repository['name']
+        repoditory_owner = repository['owner']['login']
+        result_trending_list.append({'repo_name': str(repository_name),
+                                     'repo_owner': str(repoditory_owner),
+                                     'stars': repository['stargazers_count'],
+                                     'issues': repository['open_issues'],
+                                     'url': repository['html_url']
+                                     })
+    return result_trending_list
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    query_search_url = '{0}/repos/{1}/{2}/issues'.format(GITHUB_API_URI,
-                                                         repo_owner, repo_name)
+    github_api_uri = 'https://api.github.com'
+    query_search_url = '{0}/repos/{1}/{2}/issues'.format(github_api_uri,
+                                                         repo_owner,
+                                                         repo_name)
     issues_json_data = requests.get(query_search_url).json()
-    open_issues = 0
-    for issue in issues_json_data:
-        if issue['state'] == 'open':
-            open_issues += 1
-    return open_issues
+    number_of_open_issues = len([i for i, x in enumerate(issues_json_data) if x['state'] == 'open'])
+    return number_of_open_issues
 
 
 def print_result_to_console():
@@ -47,15 +44,17 @@ def print_result_to_console():
     for index, repo in enumerate(top_repositories_list):
         good_choice_label = ''
         if not repo['issues']:
-            good_choice_label = ' Try it!'
-        print('{0:2} {1:70} {2:3} stars {3:2} issues'.format(index + 1,
-                                                             repo['url'] + good_choice_label,
-                                                             repo['stars'],
-                                                             repo['issues']))
+            good_choice_label = 'Try it!'
+        print('{0:2} {4:7} {1:70} {2:5} stars {3:2} issues'.format(index + 1,
+                                                                   repo['url'],
+                                                                   repo['stars'],
+                                                                   repo['issues'],
+                                                                   good_choice_label))
 
 
 if __name__ == '__main__':
-    week_earlier_date = get_offset_date(date.today(), '-7')
+    date_offset_in_days = -7
+    week_earlier_date = get_offset_date(date.today(), date_offset_in_days)
     number_of_results = 20
     top_repositories_list = get_trending_repositories(week_earlier_date,
                                                       number_of_results)
